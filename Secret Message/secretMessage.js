643d1ae21256fd85c45e950ea83470cffbb94b64
nicmac <nick.mac.dev@outlook.com>
2025-02-18 08:27:33 -0400

    - Updated URL for fetching the Google docs document - Updated data extraction and parsing logic for table rows - Consolidated character and coordinate extraction into a single array of objects - Improved grid initialization and character placement in the 2d array

--- Secret Message/secretMessage.js
+++ Secret Message/secretMessage.js
@@ -1,63 +1,39 @@
 // URL of the Google Docs document to fetch
-const url = 'https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub';
-
-const xCoordinates = []
-const yCoordinates = []
-const characters = []
-let gridArray = []
-const maxCols = 4
-const maxRows = 3
-let gridObj = {}
+// const url = 'https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub';
+const url = 'https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub'
+let rowObjects = []
+let maxCols = 94
+let maxRows = 7
 
 /**
  * Fetch data from a Google Docs document, parse it, and generate a secret message.
  * This function retrieves the document and extracts characters and their x/y coordinates
  */
-const getData = async () => {
+const getData = () => {
 
     // Fetch the document data from the URL
-    fetch(url)         
+    fetch(url)
         .then(response => response.text())
         .then(data => {
             const parser = new DOMParser()                              // Create a new DOM parser
-            const doc = parser.parseFromString(data, 'text/html')       // Parse the document as HTML
-            const row = doc.getElementsByTagName("tr")                  // Get all table rows from the document
+            const doc = parser.parseFromString(data, 'text/html')       // Parse the document as HTML            
+            const tableRows = doc.getElementsByTagName("tr")
 
-            // Get the characters and their x/y coordinates from the document 
-            for (let rowIndex = 1; rowIndex < row.length; rowIndex++) {
-                let currentRow = row[rowIndex].textContent
+            for (let i = 1; i < tableRows.length; i++) {
+                const currentRow = tableRows[i]
+                const tableData = currentRow.getElementsByTagName("td")
 
-                for (let colIndex = 0; colIndex < currentRow.length; colIndex++) {
-                    let currentCol = currentRow[colIndex]
-
-                    switch (colIndex) {
-                        case 0:
-                            x = `${currentCol}`         // Extract x-coordinate
-                            break
-                        case 1:
-                            char = `${currentCol}`      // Extract character
-                            break
-                        case 2:
-                            y = `${currentCol}`         // Extract y-coordinate
-                            break
-                        default:
-                            "invalid"
-                    }
+                let rowObject = {
+                    x: tableData[0].textContent,
+                    char: tableData[1].textContent,
+                    y: tableData[2].textContent
                 }
 
-                // Define an object referencing the current character and its coordinates
-                let rowObj = {
-                    char: `${char}`,
-                    x: `${x}`,
-                    y: `${y}`
-                }
-                
-                gridArray.push(rowObj)      // Add the current row object to an array 
+                rowObjects.push(rowObject)
             }
 
-            // Add the array of row objects to the main grid object & call the function to generate the secret message
-            Object.assign(gridObj, gridArray)
-            getSecretMessage(gridObj)           
+            getSecretMessage(rowObjects)
+
         })
         .catch(error => console.error('Error: ', error))
 }
@@ -67,40 +43,59 @@ const getData = async () => {
  * This function processes a grid of objects, each containing a char and its x/y coordinates,
  * arranges the characters into a 2D array, and appends the formatted results to the HTML doc
  * 
- * @param {Array} grid - Array of objects, each containing a char and its x/y coordinates 
+ * @param {Array} rowObjects - Array of objects, each containing a char and its x/y coordinates 
  */
-const getSecretMessage = async (grid) => {
-    
+const getSecretMessage = async (rowObjects) => {
+
+    let x = 0
+    let y = 0
+    let char = " "
+
     let secretMessageArray = Array.from({ length: maxRows }, () => Array(maxCols).fill(" "))        // Initialize a 2D array with empty spaces
-
-    // Iterate over the grid of objects, each containing a character and its x/y coordinates
-    for (let rowIndex in grid) {
-
-        // Extract character and its x/y coordinates from the current row object
-        let rowObj = grid[rowIndex]
-        let char = rowObj.char
-        let x = rowObj.x
-        let y = rowObj.y
-        
-        secretMessageArray[y][x] = char     // Place the character in the corresponding position in the secretMessage array 
-    }
     
-    secretMessageArray.reverse()            // Reverse the array to get correct layout for the secret message
-    
-    // Append characters from the secretMessageArray to the HTML document, formatting them into rows and cols
-    let colIndex = 0
-    secretMessageArray.forEach(row => {
+    rowObjects.forEach(row => {
+        x = row.x
+        y = row.y
+        char = row.char
+        // console.log("x: ", x, "y: ", y, "char", char)
+        secretMessageArray[y][x] = char
+    })
 
-        row.forEach(col => {
-            document.body.innerHTML += `${col}`
-            colIndex++
+    // console.log("x: ", x, "y: ", y, "char", char)
+    // secretMessageArray[y][x] = char
 
-            if (colIndex === maxCols) {
-                colIndex = 0
-                document.body.innerHTML += '<br>'
-            }
-        })
-    });
+    console.log(secretMessageArray)
+
+
+
+    // // Iterate over the grid of objects, each containing a character and its x/y coordinates
+    // for (let rowIndex in rowObjects) {
+
+    //     // Extract character and its x/y coordinates from the current row object
+    //     let rowObj = grid[rowIndex]
+    //     let char = rowObj.char
+    //     let x = rowObj.x
+    //     let y = rowObj.y
+
+    //     secretMessageArray[y][x] = char     // Place the character in the corresponding position in the secretMessage array 
+    // }
+
+    // secretMessageArray.reverse()            // Reverse the array to get correct layout for the secret message
+
+    // // Append characters from the secretMessageArray to the HTML document, formatting them into rows and cols
+    // let colIndex = 0
+    // secretMessageArray.forEach(row => {
+
+    //     row.forEach(col => {
+    //         document.body.innerHTML += `${col}`
+    //         colIndex++
+
+    //         if (colIndex === maxCols) {
+    //             colIndex = 0
+    //             document.body.innerHTML += '<br>'
+    //         }
+    //     })
+    // });
 }
 
 getData()
